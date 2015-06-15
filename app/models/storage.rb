0add8d819464280c5cd97fc43ec1e88c592a859d
sadaf <sadaf2605@gmail.com>
2015-06-15 14:56:35 +0600

    storage compatibility check done

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,3 +1,16 @@
 class Storage < ActiveRecord::Base
 	has_many :builds
+
+	def supports_case(pc_case)
+		if self.form_factor=="3.5\""
+			return pc_case.EXTERNAL_3_5_BAYS >0 || INTERNAL_3_5_BAYS >0
+		else
+			return false
+		end
+	end
+
+	def supports_motherboard(motherboard)
+		return motherboard.supports_storage(self)
+	end
+
 end
