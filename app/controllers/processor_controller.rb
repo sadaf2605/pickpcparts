db696f35d19c22fc062ca7e2b6f8bb519072f616
sadaf <sadaf2605@gmail.com>
2015-06-19 16:37:21 +0600

    working processor

--- app/controllers/processor_controller.rb
+++ app/controllers/processor_controller.rb
@@ -35,14 +35,14 @@ class ProcessorController < ApplicationController
     @processor_data_protection = Processor::DataProtection.create(processor_data_protection_params)
     @processor_platform_protection = Processor::PlatformProtection.create(processor_platform_protection_params)
 
-    @processor_processor.performance = @processor_performance
-    @processor_processor.memory = @processor_memory
-    @processor_processor.graphic = @processor_graphic
-    @processor_processor.advanced_technology = @processor_advanced_technology
-    @processor_processor.expansion = @processor_expansion
-    @processor_processor.package = @processor_package
-    @processor_processor.data_protection = @processor_data_protection
-    @processor_processor.platform_protection = @processor_platform_protection
+    @processor_processor.processor_performance = @processor_performance
+    @processor_processor.processor_memory = @processor_memory
+    @processor_processor.processor_graphic = @processor_graphic
+    @processor_processor.processor_advanced_technology = @processor_advanced_technology
+    @processor_processor.processor_expansion = @processor_expansion
+    @processor_processor.processor_package = @processor_package
+    @processor_processor.processor_data_protection = @processor_data_protection
+    @processor_processor.processor_platform_protection = @processor_platform_protection
 
     respond_to do |format|
       if @processor_processor.save
@@ -87,38 +87,38 @@ class ProcessorController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def processor_processor_params
-    params.require(:processor_processor).permit(:processor_number, :cache_in_MB, :DMI2_in_GTs, :instruction_set_in_bit, :instruction_set_extensions, :embedded_options_available, :lithography_in_nm, :scalability)
+    params.permit(:processor_number, :cache_in_MB, :DMI2_in_GTs, :instruction_set_in_bit, :instruction_set_extensions, :embedded_options_available, :lithography_in_nm, :scalability)
   end
 
   def processor_performance_params
-    params.require(:processor_performance).permit(:no_of_cores, :no_of_threads, :processor_base_frequency_in_GHz, :thermal_design_power_in_W)
+    params.require(:processor_processor).permit(:no_of_cores, :no_of_threads, :processor_base_frequency_in_GHz, :thermal_design_power_in_W)
   end
   def processor_memory_params
-    params.require(:processor_memory).permit(:max_memory_size_GB, :max_no_of_memory_channels, :max_memory_bandwidth_in_GBs, :ecc_memory_supported)
+    params.require(:processor_processor).permit(:max_memory_size_GB, :max_no_of_memory_channels, :max_memory_bandwidth_in_GBs, :ecc_memory_supported)
   end
 
   def processor_graphic_params
-    params.require(:processor_graphic).permit(:processor_graphics, :graphics_base_frequency_in_MHz, :graphics_max_dynamic_frequency_GHz, :graphics_video_max_memory_in_GB, :quick_sync_video, :wireless_display, :clear_video_hd_technology, :no_of_display_supported)
+    params.require(:processor_processor).permit(:processor_graphics, :graphics_base_frequency_in_MHz, :graphics_max_dynamic_frequency_GHz, :graphics_video_max_memory_in_GB, :quick_sync_video, :wireless_display, :clear_video_hd_technology, :no_of_display_supported)
   end
 
   def processor_expansion_params
-    params.require(:processor_extension).permit(:PCI_express_revision, :max_no_of_PCI_express_lanes)
+    params.require(:processor_processor).permit(:PCI_express_revision, :max_no_of_PCI_express_lanes)
   end
 
   def processor_package_params
-    params.require(:processor_package).permit(:max_cpu_configuration, :TCASE_in_C, :package_width_in_mn, :package_length_in_mn, :graphics_and_imc_lithography_in_nm, :low_halogen_options_available)
+    params.require(:processor_processor).permit(:max_cpu_configuration, :TCASE_in_C, :package_width_in_mn, :package_length_in_mn, :graphics_and_imc_lithography_in_nm, :low_halogen_options_available)
   end
 
   def processor_advanced_technology_params
-    params.require(:processor_advanced_technology).permit(:turbo_boost_technology, :vPro_technology, :hyper_threading_technology, :virtualization_technology_vtx, :virtualization_technology_for_directed_IO_vtd, :vtx_with_extended_page_tables_ept, :TSX_NI, :intel_64, :idle_states, :enhanced_speedstep_technology, :thermal_monitoring_technologies, :virtualization_technology_for_itanium_vti, :stable_image_platform_program_SIPP)
+    params.require(:processor_processor).permit(:turbo_boost_technology, :vPro_technology, :hyper_threading_technology, :virtualization_technology_vtx, :virtualization_technology_for_directed_IO_vtd, :vtx_with_extended_page_tables_ept, :TSX_NI, :intel_64, :idle_states, :enhanced_speedstep_technology, :thermal_monitoring_technologies, :virtualization_technology_for_itanium_vti, :stable_image_platform_program_SIPP)
   end
 
   def processor_data_protection_params
-    params.require(:processor_data_protection).permit(:AES_new_nstructions,:secure_key)
+    params.require(:processor_processor).permit(:AES_new_nstructions,:secure_key)
   end
 
   def processor_platform_protection_params
-    params.require(:processor_platform_protection).permit(:trusted_execution_technology, :execute_disable_bit)
+    params.require(:processor_processor).permit(:trusted_execution_technology, :execute_disable_bit)
   end
 
 
--- app/models/processor/processor.rb
+++ app/models/processor/processor.rb
@@ -1,19 +1,19 @@
 class Processor::Processor < ActiveRecord::Base
 
-  has_one :processor_performance, :class_name => 'Processor::Performance'
+  belongs_to :processor_performance, :class_name => 'Processor::Performance'
   belongs_to :processor_memory, :class_name => 'Processor::Memory'
-  has_one :processor_graphic, :class_name => 'Processor::Graphic'
-  has_one :processor_advanced_technology, :class_name => 'Processor::AdvancedTechnology'
-  has_one :processor_data_protection, :class_name => 'Processor::DataProtection'
-  has_one :processor_expansion, :class_name => 'Processor::Expansion'
-  has_one :processor_package, :class_name => 'Processor::Package'
-  has_one :processor_platform_protection, :class_name => 'Processor::PlatformProtection'
+  belongs_to :processor_graphic, :class_name => 'Processor::Graphic'
+  belongs_to :processor_advanced_technology, :class_name => 'Processor::AdvancedTechnology'
+  belongs_to :processor_data_protection, :class_name => 'Processor::DataProtection'
+  belongs_to :processor_expansion, :class_name => 'Processor::Expansion'
+  belongs_to :processor_package, :class_name => 'Processor::Package'
+  belongs_to :processor_platform_protection, :class_name => 'Processor::PlatformProtection'
 
 
   attr_accessor :no_of_cores, :no_of_threads, :processor_base_frequency_in_GHz, :thermal_design_power_in_W
   attr_accessor :max_memory_size_GB, :max_no_of_memory_channels, :max_memory_bandwidth_in_GBs, :ecc_memory_supported
 
-  attr_accessor :processor_graphics, :graphics_base_frequency_in_MHz, :graphics_max_dynamic_frequency_GHz, :graphics_video_max_memory_in_GB, :quick_sync_video, :wireless_display, :clear_video_hd_technology, :no_of_display_supported
+  attr_accessor :processor_graphics, :graphics_base_frequency_in_MHz, :graphics_max_dynamic_frequency_GHz, :graphics_video_max_memory_in_GB, :quick_sync_video, :wireless_display, :clear_video_hd_technology, :no_of_displays_supported
   attr_accessor :PCI_express_revision, :max_no_of_PCI_express_lanes
   attr_accessor :max_cpu_configuration, :TCASE_in_C, :package_width_in_mn, :package_length_in_mn, :graphics_and_imc_lithography_in_nm, :low_halogen_options_available
 
