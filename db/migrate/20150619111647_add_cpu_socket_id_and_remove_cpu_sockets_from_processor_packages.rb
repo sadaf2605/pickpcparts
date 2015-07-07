5b48771597c9842cb5ea5b2a2922c852eb70a9ae
sadaf <sadaf2605@gmail.com>
2015-07-07 23:13:21 +0600

    removed a migration

--- db/migrate/20150619111647_add_cpu_socket_id_and_remove_cpu_sockets_from_processor_packages.rb
+++
@@ -1,6 +0,0 @@
-class AddCpuSocketIdAndRemoveCpuSocketsFromProcessorPackages < ActiveRecord::Migration
-  def change
-    add_column :processor_packages, :cpu_socket, :integer
-    remove_column :processor_packages, :cpu_socket
-  end
-end
