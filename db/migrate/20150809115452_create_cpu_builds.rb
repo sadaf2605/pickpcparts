5400eeac9a7a3e93148d1c736d966fc9c071cf57
sadaf <sadaf2605@gmail.com>
2015-08-17 02:45:28 +0600

    heroku migration fix

--- db/migrate/20150809115452_create_cpu_builds.rb
+++ db/migrate/20150809115452_create_cpu_builds.rb
@@ -1,5 +1,12 @@
 class AddProductIdToCpu < ActiveRecord::Migration
   def change
+    
+    create_table :cpu_builds do |t|
+      t.integer :cpu_id
+      t.integer :build_id
+
+      t.timestamps null: false
+    end
     add_column :cpus, :product_id, :integer
   end
 end
