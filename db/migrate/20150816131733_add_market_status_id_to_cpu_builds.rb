68d4a2c2948586221592f66766cad2a8f52b27e7
sadaf <sadaf2605@gmail.com>
2015-08-17 02:39:22 +0600

    heroku migration fix

--- db/migrate/20150816131733_add_market_status_id_to_cpu_builds.rb
+++ db/migrate/20150816131733_add_market_status_id_to_cpu_builds.rb
@@ -1,11 +1,5 @@
 class AddMarketStatusIdToCpuBuilds < ActiveRecord::Migration
   def change
-    create_table :cpu_builds do |t|
-        t.integer :cpu_id
-        t.integer :build_id
-  
-        t.timestamps null: false
-      end
     add_column :cpu_builds, :market_status_id, :integer
     
   end
