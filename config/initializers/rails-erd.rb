34701f30eadb9e3c6b24124fb46f425a0282dce2
sadaf <sadaf2605@gmail.com>
2015-08-16 19:16:57 +0600

    featured blog

--- config/initializers/rails-erd.rb
+++ config/initializers/rails-erd.rb
@@ -6,8 +6,8 @@ domain = RailsERD::Domain.generate
 # Which entities do we have? Each entity corresponds to a model.
 #domain.entities=domain.entities + RailsERD::Domain::Entity.from_models( ActiveRecord::Base.descendants.select{|a| a.parent==Blogit})
 
-spec = Gem::Specification.find_by_name("cucumber")
-gem_root = spec.gem_dir
-gem_lib = gem_root + "/lib"
+#spec = Gem::Specification.find_by_name("cucumber")
+#gem_root = spec.gem_dir
+#gem_lib = gem_root + "/lib"
 
 # Blogit.constants.collect{|k| Blogit.const_get(k)}.select {|k| k.is_a? Class and k.extend? ActiveRecord::Base }
