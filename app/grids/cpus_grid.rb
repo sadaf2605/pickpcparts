e2a324d4488a5a3325b79f1e8573924da9e4ba67
sadaf <sadaf2605@gmail.com>
2015-08-25 13:59:04 +0600

    datagrid complete for cpu

--- app/grids/cpus_grid.rb
+++ app/grids/cpus_grid.rb
@@ -10,10 +10,9 @@ class CpusGrid
   end
   Cpu.columns.select {|s| !s.name.to_s.end_with? "_at" }.each do |c|
     filter(c.name, Cpu.columns_hash[c.name].type)
+    column(c.name,:mandatory => if [:model,:speed,:cores].include? c.name.to_sym then true else false end )
   end
+    
+  column_names_filter(:header => "Extra Columns", checkboxes: true)
 
-
-  column(:model)
-  column(:speed)
-  column(:cores)
 end
